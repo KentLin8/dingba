2a8381004b619079e7455b3316413031b2eca44e
panda <a17877yun@gmail.com>
2013-12-16 05:44:47 +0800

    FIX BUG

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,11 @@ module DingBa
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    #config.time_zone = 'Taipei'
+    #config.i18n.default_locale = "zh-TW"
+
     config.time_zone = 'Taipei'
-    config.i18n.default_locale = "zh-TW"
+    config.active_record.default_timezone = :local
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
