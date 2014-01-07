87f30e0b297c2e06b28eb6bf8a7076c8003ac2b6
panda <a17877yun@gmail.com>
2014-01-07 19:52:24 +0800

    try to fix facebook login bug

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, :kind => 'Facebook') if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
-      redirect_to res_new_path
+      #redirect_to res_new_path
     end
   end
 
