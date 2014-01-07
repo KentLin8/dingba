75f93f636a6fd64c40ea02f59200ca8165cd0a6f
panda <a17877yun@gmail.com>
2014-01-07 20:12:22 +0800

    try to fix facebook login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,10 +11,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => 'Facebook') if is_navigational_format?
+      #set_flash_message(:notice, :success, :kind => 'Facebook') if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
-      #redirect_to res_new_path
+      render res_new_path
     end
   end
 
@@ -23,7 +23,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', :kind => 'Google'
+      #flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', :kind => 'Google'
       sign_in_and_redirect @user, :event => :authentication
     else
       session['devise.google_data'] = request.env['omniauth.auth']
