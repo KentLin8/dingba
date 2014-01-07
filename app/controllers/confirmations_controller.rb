5b5a5b09c7554e078993af9dc8bb7d71c6dc2d00
panda <a17877yun@gmail.com>
2014-01-07 22:45:14 +0800

    modify resend email no this mail or alreay login view

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -5,6 +5,24 @@ class ConfirmationsController < Devise::ConfirmationsController
     render :layout => false
   end
 
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      respond_with({}, :location => after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      user = self.resource
+      if user.id.blank?
+        flash.now[:alert] = '沒有此 E-Mail'
+        render 'devise/sessions/booker_new'
+        return
+      else
+        redirect_to booker_manage_index_path
+      end
+    end
+  end
+
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
