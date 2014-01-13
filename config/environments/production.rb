52d18c13c93066f32ae658726be3c874ad3fce4d
kent <kentlin@ppolis.com>
2014-01-13 22:17:34 +0800

    remove nilclass overwrite code! please dont put such evil cord in our website!!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,8 @@ DingBa::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'dingba-admin@codream.tw'}
+  config.action_mailer.default_url_options = { :host => 'http://dingba.codream.tw' }
 
-
-
-  #config.action_mailer.default_url_options = { :host => 'http://dingba.codream.tw' }
   #ActionMailer::Base.smtp_settings = {
   #    :address        => "smtp.gmail.com",
   #    :port           => 587,
