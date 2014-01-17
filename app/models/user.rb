90f4d9a3535a359046fb507d334d1964ced26a53
panda <a17877yun@gmail.com>
2014-01-17 19:29:04 +0800

    try to fix facebook token

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
     end
     user = User.where(:email => auth.info.email).first
     unless user
-      user = User.new(name:auth.extra.raw_info.name,
+      user = User.create!(name:auth.extra.raw_info.name,
                       provider:auth.provider,
                       #uid:auth.uid,
                       email:auth.info.email,
@@ -32,8 +32,7 @@ class User < ActiveRecord::Base
 
       #user.skip_confirmation_notification!
       user.skip_confirmation!
-      user.save
-      user.send_confirmation_instructions
+      user.save!
     end
     user
   end
@@ -46,7 +45,7 @@ class User < ActiveRecord::Base
     user = User.where(:email => data["email"]).first
 
     unless user
-      user = User.new(name: data["name"],
+      user = User.create!(name: data["name"],
                       email: data["email"],
                       provider: "google",
                       password: Devise.friendly_token[0,20],
@@ -54,8 +53,7 @@ class User < ActiveRecord::Base
 
       #user.skip_confirmation_notification!
       user.skip_confirmation!
-      user.save
-      user.send_confirmation_instructions
+      user.save!
     end
     user
   end
