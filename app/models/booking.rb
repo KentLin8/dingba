ffbc4bb4f1b5cf4429977442010bdbcc35834a22
panda <a17877yun@gmail.com>
2014-01-21 16:51:01 +0800

    fix guest booking error

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,9 +2,9 @@ class Booking < ActiveRecord::Base
   OUT_OF_LENGTH = '資料長度超過限制'
   NOT_EMPTY = '不能空白'
   MUST_BE_INTEGER = '必須是整數'
-  validates :user_id, :presence => { :message => "使用者編號，" + NOT_EMPTY } ,
-                      :length => { :maximum => 11, :message => "使用者編號，" + OUT_OF_LENGTH } ,
-                      :numericality => { :only_integer => true, :message => "使用者編號，" + MUST_BE_INTEGER }
+  #validates :user_id, :presence => { :message => "使用者編號，" + NOT_EMPTY } ,
+  #                    :length => { :maximum => 11, :message => "使用者編號，" + OUT_OF_LENGTH } ,
+  #                    :numericality => { :only_integer => true, :message => "使用者編號，" + MUST_BE_INTEGER }
   validates :restaurant_id, :presence => { :message => "餐廳編號，" + NOT_EMPTY } ,
                             :length => { :maximum => 11, :message => "餐廳編號，" + OUT_OF_LENGTH } ,
                             :numericality => { :only_integer => true, :message => "餐廳編號，" + MUST_BE_INTEGER }
